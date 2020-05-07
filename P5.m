012b77a015b7a91c0de086ec282004f5adc2b711
David Flores Ruiz <ie717807@iteso.mx>
2020-05-07 17:35:42 -0500

    TFourier de todas las señales ROJO e IR, Fs=25Hz

--- P5.m
+++ P5.m
@@ -38,6 +38,40 @@ t3 = 0:step:(step*(sizeOxi3-1)); % Duración de n muestras en segundos: Oxi3
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%% TRANSFORMADAS DE FOURIER %%%%%%%%%%%%%%%%%%%%%%%%%
+nfft1 = sizeOxi1;         % el numero de puntos de la fft
+step1_W = Fs/(nfft1-1); % frecuencia de muestreo / el numero de puntos de TF
+
+% Construccion del vector de frecuencias para usar en plot()
+dom1_W  = 0 : step1_W : Fs; % Escala de frecuencia en "Hz"
+
+% Primero se obtiene la magnitud de la transformada de Fourier FFT; de
+% manera que el largo de la FFT sea nfft 
+X_ir1_w   = abs( fft(Struct_oxi1.x_ir, nfft1) );
+X_red1_w  = abs( fft(Struct_oxi1.x_red,nfft1) );
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - %
+nfft2 = sizeOxi2;         % el numero de puntos de la fft
+step2_W = Fs/(nfft2-1); % frecuencia de muestreo / el numero de puntos de TF
+
+% Construccion del vector de frecuencias para usar en plot()
+dom2_W  = 0 : step2_W : Fs; % Escala de frecuencia en "Hz"
+
+% Primero se obtiene la magnitud de la transformada de Fourier FFT; de
+% manera que el largo de la FFT sea nfft 
+X_ir2_w   = abs( fft(Struct_oxi2.x_ir, nfft2) );
+X_red2_w  = abs( fft(Struct_oxi2.x_red,nfft2) );
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - %
+nfft3 = sizeOxi3;         % el numero de puntos de la fft
+step3_W = Fs/(nfft3-1); % frecuencia de muestreo / el numero de puntos de TF
+
+% Construccion del vector de frecuencias para usar en plot()
+dom3_W  = 0 : step3_W : Fs; % Escala de frecuencia en "Hz"
+
+% Primero se obtiene la magnitud de la transformada de Fourier FFT; de
+% manera que el largo de la FFT sea nfft 
+X_ir3_w   = abs( fft(Struct_oxi3.x_ir, nfft3) );
+X_red3_w  = abs( fft(Struct_oxi3.x_red,nfft3) );
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
@@ -86,3 +120,53 @@ xlabel('tiempo (s)') % Nombre del eje X
 ylabel('Amplitud Am')% Nombre del eje Y
 grid on % Cuadrícula Activada
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - %
+% Se GRAFICA las TF para cada uno de los 3 archivos originales
+figure(2);  % TF de cada señal de cada archivo
+subplot(2, 3, 1) % Varias Graficas (2 filas, 3 columnas, 1ra posición)
+plot(dom1_W,X_ir1_w)  % Señal original: Oxi1
+xlim([0 2])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+title('TFourier: IR - señal 1') % Titulo del gráfico
+xlabel('frecuencia (w) en Hz') % Nombre del eje X
+ylabel('Amplitud Am')          % Nombre del eje Y
+grid on % Cuadrícula Activada
+
+subplot(2, 3, 4) % Varias Graficas (2 filas, 3 columnas, 4ta posición)
+plot(dom1_W,X_red1_w)  % Señal original: Oxi1
+xlim([0 2])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+title('TFourier: RED - señal 1 (BPM)') % Titulo del gráfico
+xlabel('frecuencia (w) en Hz') % Nombre del eje X
+ylabel('Amplitud Am')          % Nombre del eje Y
+grid on % Cuadrícula Activada
+
+subplot(2, 3, 2) % Varias Graficas (2 filas, 3 columnas, 2da posición)
+plot(dom2_W,X_ir2_w)  % Señal original: Oxi2
+xlim([0 2])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+title('TFourier: IR - señal 2') % Titulo del gráfico
+xlabel('frecuencia (w) en Hz') % Nombre del eje X
+ylabel('Amplitud Am')          % Nombre del eje Y
+grid on % Cuadrícula Activada
+
+subplot(2, 3, 5) % Varias Graficas (2 filas, 3 columnas, 5ta posición)
+plot(dom2_W,X_red2_w)  % Señal original: Oxi2
+xlim([0 2])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+title('TFourier: RED - señal 2 (BPM)') % Titulo del gráfico
+xlabel('frecuencia (w) en Hz') % Nombre del eje X
+ylabel('Amplitud Am')          % Nombre del eje Y
+grid on % Cuadrícula Activada
+
+subplot(2, 3, 3) % Varias Graficas (2 filas, 3 columnas, 3ra posición)
+plot(dom3_W,X_ir3_w)  % Señal original: Oxi3
+xlim([0 2])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+title('TFourier: IR - señal 3') % Titulo del gráfico
+xlabel('frecuencia (w) en Hz') % Nombre del eje X
+ylabel('Amplitud Am')          % Nombre del eje Y
+grid on % Cuadrícula Activada
+
+subplot(2, 3, 6) % Varias Graficas (2 filas, 3 columnas, 6ta posición)
+plot(dom3_W,X_red3_w)  % Señal original: Oxi3
+xlim([0 2])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+title('TFourier: RED - señal 3 (BPM)') % Titulo del gráfico
+xlabel('frecuencia (w) en Hz') % Nombre del eje X
+ylabel('Amplitud Am')          % Nombre del eje Y
+grid on % Cuadrícula Activada
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
