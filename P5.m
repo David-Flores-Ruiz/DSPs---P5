f87231d0fc8655bea38ed8271f011896391dde00
David Flores Ruiz <ie717807@iteso.mx>
2020-05-08 21:30:14 -0500

    Vemos valores de BeatPerSecond en grafica de TF, falta sacarlos en código

--- P5.m
+++ P5.m
@@ -8,8 +8,10 @@ clear all;
 close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  DEFINES  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-MORE_POINTS = 10;     % Factor para definir mayor número de puntos en la TF
-ZOOM_TF = 1;    % Para un acercamiento a la gráfica, esperamos f0 = 1.25Hz
+MORE_POINTS = 1;    % Factor para definir mayor número de puntos en la TF
+ZOOM_TF1 = 3;  % Para un acercamiento a la gráfica, esperamos f0 = 1.25Hz
+ZOOM_TF2 = 3;  % Para un acercamiento a la gráfica, esperamos f0 = 1.25Hz
+ZOOM_TF3 = 3;  % Para un acercamiento a la gráfica, esperamos f0 = 1.25Hz
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -75,6 +77,27 @@ X_red3_w  = abs( fft(Struct_oxi3.x_red,nfft3) );
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
+fprintf('Valores TF red1: 1 - 10');
+X_red1_w(1:10)
+fprintf(' - Eliminamos los primeros  5 componentes de TF \n');
+X_red1_w(1:5) = 0;
+fprintf('Valores TF red1: 1 - 10');
+X_red1_w(1:10)
+
+fprintf('Valores TF red2: 1 - 10 \n ');
+X_red2_w(1:10);
+fprintf(' - Eliminamos los primeros 35 componentes de TF \n');
+X_red2_w(1:35) = 0;
+fprintf('Valores TF red2: 1 - 10 \n\n');
+X_red2_w(1:10);
+
+fprintf('Valores TF red3: 1 - 10 \n ');
+X_red3_w(1:10);
+fprintf(' - Eliminamos los primeros 40 componentes de TF \n');
+X_red3_w(1:40) = 0;
+fprintf('Valores TF red3: 1 - 10 \n');
+X_red3_w(1:10);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Se GRAFICA las salidas de cada uno de los 3 archivos originales
@@ -125,15 +148,15 @@ grid on % Cuadrícula Activada
 figure(2);  % TF de cada señal de cada archivo
 subplot(2, 3, 1) % Varias Graficas (2 filas, 3 columnas, 1ra posición)
 plot(dom1_W,X_ir1_w)  % Señal original: Oxi1
-xlim([0 ZOOM_TF])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+xlim([0 ZOOM_TF1])  % Para hecer un zoom al espectro, ver los armonicos y la f0
 title('TFourier: IR - señal 1') % Titulo del gráfico
 xlabel('frecuencia (w) en Hz') % Nombre del eje X
 ylabel('Amplitud Am')          % Nombre del eje Y
 grid on % Cuadrícula Activada
 
 subplot(2, 3, 4) % Varias Graficas (2 filas, 3 columnas, 4ta posición)
-plot(dom1_W,X_red1_w)  % Señal original: Oxi1
-xlim([0 ZOOM_TF])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+plot(dom1_W,X_red1_w, '-o')  % Señal original: Oxi1
+xlim([0 ZOOM_TF1])  % Para hecer un zoom al espectro, ver los armonicos y la f0
 title('TFourier: RED - señal 1 (BPM)') % Titulo del gráfico
 xlabel('frecuencia (w) en Hz') % Nombre del eje X
 ylabel('Amplitud Am')          % Nombre del eje Y
@@ -141,15 +164,15 @@ grid on % Cuadrícula Activada
 
 subplot(2, 3, 2) % Varias Graficas (2 filas, 3 columnas, 2da posición)
 plot(dom2_W,X_ir2_w)  % Señal original: Oxi2
-xlim([0 ZOOM_TF])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+xlim([0 ZOOM_TF2])  % Para hecer un zoom al espectro, ver los armonicos y la f0
 title('TFourier: IR - señal 2') % Titulo del gráfico
 xlabel('frecuencia (w) en Hz') % Nombre del eje X
 ylabel('Amplitud Am')          % Nombre del eje Y
 grid on % Cuadrícula Activada
 
 subplot(2, 3, 5) % Varias Graficas (2 filas, 3 columnas, 5ta posición)
-plot(dom2_W,X_red2_w)  % Señal original: Oxi2
-xlim([0 ZOOM_TF])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+plot(dom2_W,X_red2_w, '-o')  % Señal original: Oxi2
+xlim([0 ZOOM_TF2])  % Para hecer un zoom al espectro, ver los armonicos y la f0
 title('TFourier: RED - señal 2 (BPM)') % Titulo del gráfico
 xlabel('frecuencia (w) en Hz') % Nombre del eje X
 ylabel('Amplitud Am')          % Nombre del eje Y
@@ -157,15 +180,15 @@ grid on % Cuadrícula Activada
 
 subplot(2, 3, 3) % Varias Graficas (2 filas, 3 columnas, 3ra posición)
 plot(dom3_W,X_ir3_w)  % Señal original: Oxi3
-xlim([0 ZOOM_TF])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+xlim([0 ZOOM_TF3])  % Para hecer un zoom al espectro, ver los armonicos y la f0
 title('TFourier: IR - señal 3') % Titulo del gráfico
 xlabel('frecuencia (w) en Hz') % Nombre del eje X
 ylabel('Amplitud Am')          % Nombre del eje Y
 grid on % Cuadrícula Activada
 
 subplot(2, 3, 6) % Varias Graficas (2 filas, 3 columnas, 6ta posición)
-plot(dom3_W,X_red3_w)  % Señal original: Oxi3
-xlim([0 ZOOM_TF])  % Para hecer un zoom al espectro, ver los armonicos y la f0
+plot(dom3_W,X_red3_w, '-o')  % Señal original: Oxi3
+xlim([0 ZOOM_TF3])  % Para hecer un zoom al espectro, ver los armonicos y la f0
 title('TFourier: RED - señal 3 (BPM)') % Titulo del gráfico
 xlabel('frecuencia (w) en Hz') % Nombre del eje X
 ylabel('Amplitud Am')          % Nombre del eje Y
